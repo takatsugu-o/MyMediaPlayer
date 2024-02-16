ad09d0a42abdf2b559033103cf9483bdf29022e5
takatsugu <takatsugu0404@gmail.com>
2024-02-17 07:20:00 +0900

    プロジェクト ファイルを追加します。

---
+++ MyMediaPlayer/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace MyMediaPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Player = new AxWMPLib.AxWindowsMediaPlayer();
+            this.select_video = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Player
+            // 
+            this.Player.Enabled = true;
+            this.Player.Location = new System.Drawing.Point(37, 51);
+            this.Player.Name = "Player";
+            this.Player.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("Player.OcxState")));
+            this.Player.Size = new System.Drawing.Size(324, 221);
+            this.Player.TabIndex = 0;
+            // 
+            // select_video
+            // 
+            this.select_video.CausesValidation = false;
+            this.select_video.Location = new System.Drawing.Point(122, 365);
+            this.select_video.Name = "select_video";
+            this.select_video.Size = new System.Drawing.Size(75, 23);
+            this.select_video.TabIndex = 1;
+            this.select_video.Text = "select_video";
+            this.select_video.UseVisualStyleBackColor = true;
+            this.select_video.Click += new System.EventHandler(this.select_video_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 407);
+            this.Controls.Add(this.select_video);
+            this.Controls.Add(this.Player);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer Player;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button select_video;
+    }
+}
+
---
+++ MyMediaPlayer/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+using WMPLib;
+
+namespace MyMediaPlayer
+{
+    public partial class Form1 : Form
+    {
+        private bool isFullScreen;
+
+        private Size normalPlayerSize;
+        private Point normalPlayerLocation;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            // ドラッグ＆ドロップを受け入れるように設定
+            this.AllowDrop = true;
+
+            Player.fullScreen = false;
+
+            normalPlayerSize = Player.Size;
+            normalPlayerLocation = Player.Location;
+        }
+
+        private void PlayMedia(string url)
+        {
+            Player.URL = url;
+        }
+
+        private void select_video_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opf = new OpenFileDialog();
+            if (opf.ShowDialog() == DialogResult.OK)
+            {
+                PlayMedia(opf.FileName);
+            }
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                // ファイルがドラッグされてきた場合
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                // ファイル以外がドラッグされてきた場合
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            // ドロップされたデータがファイルの場合
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                // ドロップされたファイルのパスを取得
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                // ドロップされたファイルがある場合、最初のファイルのパスを取得
+                if (files != null && files.Length > 0)
+                {
+                    string fileUrl = files[0]; // ドロップされた最初のファイルのパスを取得
+
+                    PlayMedia(fileUrl);
+                }
+            }
+        }
+
+        private Screen GetExtendedScreen()
+        {
+            foreach (Screen screen in Screen.AllScreens)
+            {
+                if (screen != Screen.PrimaryScreen && screen.Bounds != Screen.PrimaryScreen.Bounds)
+                {
+                    return screen;
+                }
+            }
+            return null;
+        }
+
+        private void ToggleScreen()
+        {
+            if (isFullScreen)
+            {
+                // 通常スクリーンモードに切り替え
+                this.FormBorderStyle = FormBorderStyle.Sizable;
+                this.WindowState = FormWindowState.Normal;
+                // メディアプレイヤーのサイズを元のサイズに戻す
+                Player.Dock = DockStyle.None;
+                Player.Size = normalPlayerSize;
+                Player.Location = normalPlayerLocation;
+
+                Player.uiMode = "Full";
+
+                //Select ボタンを有効にする
+                select_video.Enabled = true;
+                select_video.Visible = true;
+
+                //Toggle state
+                isFullScreen = false;
+            }
+            else
+            {
+                // フルスクリーンモードに切り替え
+                this.FormBorderStyle = FormBorderStyle.None;
+                this.WindowState = FormWindowState.Maximized;
+
+                // 拡張ディスプレイの情報を取得
+                Screen extendedScreen = GetExtendedScreen();
+                if (extendedScreen != null)
+                {
+                    // フォームを拡張ディスプレイに配置
+                    this.Location = extendedScreen.WorkingArea.Location;
+                    this.Size = extendedScreen.WorkingArea.Size;
+                }
+
+
+                // メディアプレイヤーのサイズをウィンドウに合わせる
+                Player.Dock = DockStyle.Fill;
+
+                Player.uiMode = "None";
+                
+                //Select ボタンを無効にする
+                select_video.Enabled = false;
+                select_video.Visible = false;
+
+                //Toggle State
+                isFullScreen = true;
+            }
+        }
+
+        private void PausePlayer()
+        {
+            if(Player.playState == WMPLib.WMPPlayState.wmppsPlaying)
+            {
+                Player.Ctlcontrols.pause();
+            }
+            else
+            {
+                Player.Ctlcontrols.play();
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            this.Focus();
+            switch (e.KeyCode)
+            {
+                case Keys.M:
+                    ToggleScreen();
+                    break;
+                case Keys.Space:
+                    PausePlayer();
+                    break;
+                case Keys.Q:
+                    Application.Exit();
+                    break;
+            }
+            
+        }
+
+        
+    }
+}
